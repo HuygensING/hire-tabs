a4a4964756003c0cd78dc2d80151a21b6fa69d5d
Gijsjan <agijsbro@gmail.com>
2015-08-10 14:05:13 +0200

    Replace browserify with watchify

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-node_modules/.bin/browserify src/index.js \
+node_modules/.bin/watchify src/index.js \
 	--detect-globals false \
 	--extension=.jsx \
 	--external classnames \
 	--external react \
+	--outfile 'node_modules/.bin/derequire > build/index.js' \
 	--standalone HireTabs \
 	--transform [ babelify --plugins object-assign ] \
-	--verbose | derequire > build/index.js
+	--verbose
